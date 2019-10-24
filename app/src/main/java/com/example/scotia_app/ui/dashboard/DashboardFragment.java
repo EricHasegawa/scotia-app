28c2594064784076bbaed3b7cc7e8b94f6f56d97
tatea <tateashby@shaw.ca>
2019-10-24 11:13:25 -0400

    Added DashboardFragment.loadInvoices and DashboardFragment.createView methods and DashboardFragment.invoices instance attribute. loadInvoices populates invoices from raw JSON data retrieved from the backend, and createView is a helper method which returns a TextView for a given invoice JSON object.

--- app/src/main/java/com/example/scotia_app/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/example/scotia_app/ui/dashboard/DashboardFragment.java
@@ -14,7 +14,16 @@ import androidx.lifecycle.ViewModelProviders;
 
 import com.example.scotia_app.R;
 
+import java.util.ArrayList;
+
+import org.json.*;
+
 public class DashboardFragment extends Fragment {
+
+    /**
+     * A List of Views corresponding to invoice previews to populate the invoice page's ScrollView.
+     */
+    private ArrayList<View> invoices = new ArrayList<>();
 
     private DashboardViewModel dashboardViewModel;
 
@@ -32,4 +41,35 @@ public class DashboardFragment extends Fragment {
         });
         return root;
     }
+
+    /**
+     * Parse raw JSON invoice data and populate invoices with the created invoices.
+     *
+     * @param rawJSON The JSON array of invoices to parse and populate scrollView with,
+     */
+    private void loadInvoices(String rawJSON) {
+        try {
+            JSONObject jsonObject = new JSONObject(rawJSON);
+            JSONArray jsonArray = jsonObject.getJSONArray("invoices");
+            for (int i = 0; i < jsonArray.length(); i++) {
+                invoices.add(createView(jsonArray.getJSONObject(i)));
+            }
+        }
+        catch (JSONException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    /**
+     * Given jsonObjectInvoice, a JSONObject containing data for a single invoice, constructs an
+     * appropriate View to be displayed within scrollView.
+     *
+     * @param jsonObjectInvoice A single invoice, in form of a JSONObject.
+     * @return A TextView containing some of the invoice's data to be displayed on this invoice
+     * preview.
+     */
+    private TextView createView(JSONObject jsonObjectInvoice) {
+        return new TextView(getContext());
+    }
+
 }

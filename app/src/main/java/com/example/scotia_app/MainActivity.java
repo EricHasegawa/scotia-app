8313ed0073dca897552081d5d4d80d60c7e740ae
salmanshah1d <s.shahid@mail.utoronto.ca>
2019-10-29 22:34:30 -0400

    Changed color of bottom navigation, set opening screen to persona selector

--- app/src/main/java/com/example/scotia_app/MainActivity.java
+++ app/src/main/java/com/example/scotia_app/MainActivity.java
@@ -1,14 +1,13 @@
 package com.example.scotia_app;
 
+import android.content.Intent;
 import android.os.Bundle;
-
-import com.google.android.material.bottomnavigation.BottomNavigationView;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,15 +15,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_invoices, R.id.navigation_notifications, R.id.navigation_profile)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(navView, navController);
+
+        // Initializes this spinner with the specified possible personas in the strings.xml file
+        final Spinner personaSelectorSpinner = findViewById(R.id.personaSelectorSpinner);
+        ArrayAdapter<CharSequence> spinnerListAdapter;
+        spinnerListAdapter = ArrayAdapter.createFromResource(this, R.array.personaList,
+                android.R.layout.simple_spinner_item);
+        spinnerListAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        personaSelectorSpinner.setAdapter(spinnerListAdapter);
+
+        Button startAppButton = (Button) findViewById(R.id.startAppButton);
+        startAppButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent switchToBottomNavigationView = new Intent(MainActivity.this,
+                        BottomNavigationActivity.class);
+                startActivity(switchToBottomNavigationView);
+            }
+        });
     }
 
 }

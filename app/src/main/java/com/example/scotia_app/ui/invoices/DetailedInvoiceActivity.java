0edb721b645e2c2012c99233a5554d79c6a79e83
EricHasegawa <eric.hasegawa@mail.utoronto.ca>
2019-10-31 11:58:04 -0400

    Added the DetailedInvoiceActivityClass starter code, and a button to return to the general invoices list.

---
+++ app/src/main/java/com/example/scotia_app/ui/invoices/DetailedInvoiceActivity.java
@@ -0,0 +1,51 @@
+package com.example.scotia_app.ui.invoices;
+
+import android.os.Bundle;
+
+import com.example.scotia_app.User;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+import android.widget.Button;
+
+import com.example.scotia_app.R;
+
+import org.json.JSONObject;
+
+public class DetailedInvoiceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        User user = getIntent().getParcelableExtra("user");
+//        Invoice invoice = getIntent().getParcelableExtra("invoice");
+        setContentView(R.layout.activity_detailed_invoice);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        configureBackButton();
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    private void configureBackButton() {
+        Button backButton = (Button) findViewById(R.id.backButton); {
+            backButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    finish();
+                }
+            });
+        }
+    }
+
+}
